d9f982257c656480ef66fe353726befb1df5a4bb
Lethabo-A <lethaboanyanwu2@gmail.com>
2024-08-12 16:16:32 +0200

    changes made to controllers

--- Program.cs
+++ Program.cs
@@ -93,13 +93,22 @@ var securityRequirement = new OpenApiSecurityRequirement
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    // Configure the HTTP request pipeline.
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API development");
+        c.RoutePrefix = string.Empty;
+    });
 
+}
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
